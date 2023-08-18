e46b7bf7a0a2181443af94935029d4b68b5ca847
Somya Bhatt <somyacu1@gmail.com>
2023-08-18 18:31:26 +0530

    using frammer instead of tilt

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -18,13 +17,9 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
+      <motion.div
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        whileHover={{ scale: 1.05 }}
       >
         <div className='relative w-full h-[230px]'>
           <img
@@ -33,18 +28,19 @@ const ProjectCard = ({
             className='w-full h-full object-cover rounded-2xl'
           />
 
-          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
-            <div
+          <motion.div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <motion.div
               onClick={() => window.open(source_code_link, "_blank")}
               className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+              whileHover={{ scale: 1.1 }}
             >
               <img
                 src={github}
                 alt='source code'
                 className='w-1/2 h-1/2 object-contain'
               />
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
         </div>
 
         <div className='mt-5'>
@@ -62,7 +58,7 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-      </Tilt>
+      </motion.div>
     </motion.div>
   );
 };
@@ -80,11 +76,11 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
+          Following projects showcase my skills and experience through
           real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          links to code repositories and live demos. It reflects my ability to
+          solve complex problems, work with different technologies, and manage
+          projects effectively.
         </motion.p>
       </div>
 
